0e8b1ee8e36eaa4b295e9759fac72e8cd92ce35a
Vera Thies <vera.thies94@gmail.com>
2020-04-13 15:41:14 +0200

    added responsive topnav

--- js/main.js
+++ js/main.js
@@ -1,21 +1,11 @@
  // dropdown menu 
 
- /* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
+}
