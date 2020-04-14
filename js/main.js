0dbe8f29a1997b800f052ae0be5d9f9e13ebdf1a
Vera Thies <vera.thies94@gmail.com>
2020-04-14 22:22:25 +0200

    hover over for desktop only, onclick for smaller screen

--- js/main.js
+++ js/main.js
@@ -9,3 +9,26 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+// pricing information: show pricing details on click for smaller screens
+
+function showBtn1() {
+  var x = document.getElementById("showMe1");
+  x.style.display = "block";
+}
+
+function showBtn2() {
+  var x = document.getElementById("showMe2");
+  x.style.display = "block";
+}
+
+function showBtn3() {
+  var x = document.getElementById("showMe3");
+  x.style.display = "block";
+}
+
+function showBtn4() {
+  var x = document.getElementById("showMe4");
+  x.style.display = "block";
+}
+
